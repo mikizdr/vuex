97e233bbba5bd41dad0a800964e5625010dae2bf
Miroslav HP <miroslav.zdravkovic@gmail.com>
2019-02-06 09:45:01 +0100

    GETTER for unregistedUsers

--- src/store.js
+++ src/store.js
@@ -13,5 +13,12 @@ export const store = new Vuex.Store({
             { id: 3, name: 'Chris', registered: false },
             { id: 4, name: 'Sven', registered: false }
         ]
+    },
+    getters: {
+        unregisteredUsers(state) {
+            return state.users.filter(user => {
+                return !user.registered;
+            })
+        }
     }
 });
